cf2a8b71560151a5f95cfab73ec5b475eba37396
xuhaixiangcan <72588599+xuhaixiangcan@users.noreply.github.com>
2025-01-18 22:02:48 +0800

    Update xhx1.c

--- xhx1.c
+++ xhx1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-# copy from xhx.cpp
-
+/* copy from xhx.cpp with rm error sentence */
+/* this is from github edited version*/
 int main() {
-  int i=10
-  printf("Hello World! /n");
-  printf("I is %d /n", i);
-  printf("this is new after SSH /n");
+  int i=10;
+  printf("Hello World! \n");
+  printf("I is %d \n", i);
+  printf("this is new after SSH \n");
   return 0;
 }
